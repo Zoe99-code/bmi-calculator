03da8b6241589b1d469109cf200080baf081a0aa
Zoe99-code <lizzystrachan99@gmail.com>
2021-05-19 11:30:16 +0200

    first commit

--- OOP/tkinter-oop.py
+++ OOP/tkinter-oop.py
@@ -1,11 +1,31 @@
-import
+from tkinter import *
+root = Tk()
+root.title("Classes in Tkinter")
+root.geometry("500x500")
+
+class Circle:
+    myresult = StringVar()
+    import math
+    def __init__(self, master):
+        self.lab1=Label(master, text="Please enter radius")
+        self.lab1.place(x=5, y=5)
+        self.myentry=Entry(master)
+        self.myentry.place(x=150, y=5)
+        self.lab2=Label(master, text="", width="50", textvariable=self.myresult)
+        self.lab2.place(x=20, y=50)
+        self.btn_area=Button(master, text="Calculate Area", command=self.area_of_circle)
+        self.btn_area.place(x=100, y=100)
+
+    def area_of_circle(self):
+        result=math.pi * int(self.myentry.get() * int(self.myentry.get()))
+        self.myresult.set(result)
 
 
 
 
-def setUp(self) -> None:
-    shape = Shapes("shape", "so many")
 
 
-    def test_init_method(self):
-        self.assertEqual(self.shape.area(), "I am a shape\nI have so many")
+
+x = Circle(root)
+root.mainloop()
+
